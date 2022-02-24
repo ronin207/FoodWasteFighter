9968c326b15a223439c996624fefd76ac46a1742
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-02-24 13:13:48 +0800

    Initial Commit

---
+++ FoodWasteFighter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 24/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodWasteFighter/FoodWasteFighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodWasteFighterApp.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 24/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodWasteFighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
