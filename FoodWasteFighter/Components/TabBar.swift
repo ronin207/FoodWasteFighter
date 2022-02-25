bf1f71e71ab7e62824d162f111d5e600f42f3aa7
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-02-25 12:08:44 +0800

    Created TabBarView

---
+++ FoodWasteFighter/Components/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 25/2/22.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
