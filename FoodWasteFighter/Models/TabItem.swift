1153a4e8fc1fdb47b6065a4960f36fc94bf7ed41
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-02-25 12:17:08 +0800

    Created TabItem model for the TabBarView

---
+++ FoodWasteFighter/Models/TabItem.swift
@@ -0,0 +1,20 @@
+//
+//  TabItem.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 25/2/22.
+//
+
+import SwiftUI
+
+struct TabItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabItem_Previews: PreviewProvider {
+    static var previews: some View {
+        TabItem()
+    }
+}
