b8cc84ed89c84190945dc4e7bdda7ce405a68acf
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-02-26 14:11:44 +0800

    Declared variables to see BlogPost with different results and customized interactive buttons (eg. Favourites, Comment). Created a glassmorphic effect on the BlogPost.

--- FoodWasteFighter/Components/BlogPost.swift
+++ FoodWasteFighter/Components/BlogPost.swift
@@ -8,57 +8,64 @@
 import SwiftUI
 
 struct BlogPost: View {
+    @State private var star: Bool = false
+    @State private var comment: Bool = false
+    
+    let name: String
+    let description: String
+    
     var body: some View {
         VStack(spacing: 0) {
             HStack(spacing: 10) {
                 Image(systemName: "person.circle")
                     .resizable()
                     .frame(width: 30, height: 30)
-                Text("ABCD Store")
+                Text(name)
                     .fontSelection(size: 25)
             }
             .padding(.horizontal)
+            .padding(.vertical, 3)
             .frame(maxWidth: .infinity, alignment: .leading)
-            .background(Color.clover, in: Rectangle())
             
             Rectangle()
                 .frame(maxWidth: .infinity)
-                .frame(height: 200)
-                .foregroundColor(.brown)
+                .frame(height: 150)
             
             VStack(spacing: 10) {
                 HStack(spacing: 20) {
                     Button {
-                        
+                        star.toggle()
                     } label: {
-                        Image(systemName: "star")
+                        Image(systemName: star ? "star.fill" : "star")
                             .resizable()
                             .frame(width: 25, height: 25)
                     }
+                    .tint(.yellow)
                     
                     Button {
-                        
+                        comment.toggle()
                     } label: {
-                        Image(systemName: "message")
+                        Image(systemName: "message.fill")
                             .resizable()
                             .frame(width: 25, height: 25)
                     }
+                    .tint(.brown)
                 }
                 .padding(.horizontal)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
                 HStack(alignment: .top, spacing: 30) {
-                    Text("ABCD Store")
+                    Text(name)
                         .fontSelection(type: .shadowsLight2, size: 18)
-                    Text("Here’s an idea for browned avocadoes- make guacamole!")
+                    Text(description)
                         .fontSelection(size: 13)
                 }
                 .padding(.horizontal)
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
             .padding(.vertical)
-            .background(Color(red: 255/255, green: 255/255, blue: 240/255), in: Rectangle())
         }
+        .background(.ultraThinMaterial)
         .cornerRadius(5.0)
     }
 }
@@ -66,14 +73,21 @@ struct BlogPost: View {
 struct BlogPost_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            LinearGradient(gradient: Gradient(colors: [.green, .mint, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
             ScrollView(.vertical, showsIndicators: false) {
                 LazyVStack(spacing: 10) {
-                    BlogPost()
-                    BlogPost()
-                    BlogPost()
+                    BlogPost(name: "ABCD Store", description: "Here’s an idea for browned avocadoes- make guacamole!")
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
+                    BlogPost(name: "Navya Ann", description: "Hey guys! we just had a graduation party and we have some extra food- link in my bio!")
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
+                    BlogPost(name: "ABCD Store", description: "Here’s an idea for browned avocadoes- make guacamole!")
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
                 }
             }
+            .padding(5)
+            .ignoresSafeArea(.container, edges: .bottom)
         }
     }
 }
