228199b8e582bf9b5e600a9933f844957e93d6ef
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-08 22:23:41 +0800

    Initial Commit

---
+++ FoodWasteFighter/Components/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 8/3/22.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
