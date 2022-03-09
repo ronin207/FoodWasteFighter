b9782589211e66a985ea84b6cb9c9773f9dd8f67
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-10 02:03:39 +0800

    Initial Commit

---
+++ FoodWasteFighter/Models/AuthenticationViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationViewModel.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 10/3/22.
+//
+
+import SwiftUI
+
+struct AuthenticationViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthenticationViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationViewModel()
+    }
+}
