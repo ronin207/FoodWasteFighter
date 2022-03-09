d71fb283518a67162e83ca492dcf9e5774dbdbd4
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-10 02:00:58 +0800

    Updated FoodWasteFighterApp file with Firebase module to configure Firebase throughout the application.

--- FoodWasteFighter/FoodWasteFighterApp.swift
+++ FoodWasteFighter/FoodWasteFighterApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FoodWasteFighterApp: App {
     init() {
         UINavigationBar.appearance().backgroundColor = UIColor.clear
         UITextField.appearance().backgroundColor = UIColor(.white.opacity(0.4))
+        setupAuthentication()
     }
     
     var body: some Scene {
@@ -22,5 +24,11 @@ struct FoodWasteFighterApp: App {
             }
             .navigationViewStyle(.stack)
         }
+    }
+}
+
+extension FoodWasteFighterApp {
+    private func setupAuthentication() {
+        FirebaseApp.configure()
     }
 }
