0d63a576ede39705ab27275d96dd2bb7304a32ae
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-10 02:22:27 +0800

    Instantiate AuthenticationViewModel with signIn and signOut capabilities.

--- FoodWasteFighter/Models/AuthenticationViewModel.swift
+++ FoodWasteFighter/Models/AuthenticationViewModel.swift
@@ -5,16 +5,61 @@
 //  Created by Takumi Otsuka on 10/3/22.
 //
 
-import SwiftUI
+import Firebase
+import GoogleSignIn
 
-struct AuthenticationViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class AuthenticationViewModel: ObservableObject {
+    @Published var state: LoginState = .signout
+    
+    enum LoginState {
+        case signin
+        case signout
     }
-}
-
-struct AuthenticationViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        AuthenticationViewModel()
+    
+    func signIn() {
+        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
+            GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        } else {
+            guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+            let configuration = GIDConfiguration(clientID: clientID)
+            
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            guard let presenting = windowScene.windows.first?.rootViewController else { return }
+            
+            GIDSignIn.sharedInstance.signIn(with: configuration, presenting: presenting) { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        }
+    }
+    
+    func signOut() {
+        GIDSignIn.sharedInstance.signOut()
+        
+        do {
+            try Auth.auth().signOut()
+            state = .signout
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func authenticateUser(for user: GIDGoogleUser?, with error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        guard let authentication = user?.authentication, let idToken = authentication.idToken else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+        
+        Auth.auth().signIn(with: credential) { [unowned self] _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                self.state = .signin
+            }
+        }
     }
 }
