6e2719ac9b4fc2519ac8be56e891b697c665864e
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-10 02:29:18 +0800

    Initial Commit

---
+++ FoodWasteFighter/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 10/3/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
