03f3323c13798acf4dbf774e5935db14063d9cdf
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-10 02:44:15 +0800

    Refactored ContentView to present either LoginView or TabView when signedIn or signedOut.

--- FoodWasteFighter/ContentView.swift
+++ FoodWasteFighter/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var authenticationvm: AuthenticationViewModel
+    
     var body: some View {
-        TabBar()
+        switch authenticationvm.state {
+        case .signin:
+            TabBar()
+        case .signout:
+            LoginView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(AuthenticationViewModel())
     }
 }
