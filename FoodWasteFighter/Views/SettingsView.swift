c06e376cb8a4263d507154581e6b697a3c069505
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-17 12:35:04 +0800

    Moved SettingsView code from ProfileView to a separate file.

--- FoodWasteFighter/Views/SettingsView.swift
+++ FoodWasteFighter/Views/SettingsView.swift
@@ -8,8 +8,93 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject private var authenticationvm: AuthenticationViewModel
+    @State private var account = false
+    @State private var privacy = false
+    @State private var help = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            DisclosureGroup(isExpanded: $account) {
+                VStack(alignment: .leading, spacing: 10) {
+                    NavigationLink(destination: {}) {
+                        Text("Personal Information")
+                    }
+                    
+                    NavigationLink(destination: {}) {
+                        Text("Language")
+                    }
+                    
+                    NavigationLink(destination: {}) {
+                        Text("Professional Account")
+                    }
+                }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+            } label: {
+                Text("Account")
+                    .foregroundColor(.primary)
+            }
+            
+            DisclosureGroup(isExpanded: $privacy) {
+                VStack(alignment: .leading, spacing: 10) {
+                    NavigationLink(destination: {}) {
+                        Text("Hide Inappropriate Language")
+                    }
+                    
+                    NavigationLink(destination: {}) {
+                        Text("Code of Conduct")
+                    }
+                }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+            } label: {
+                Text("Privacy")
+                    .foregroundColor(.primary)
+            }
+            
+            DisclosureGroup(isExpanded: $help) {
+                VStack(alignment: .leading, spacing: 10) {
+                    NavigationLink(destination: {}) {
+                        Text("FAQs")
+                    }
+                    
+                    NavigationLink(destination: {}) {
+                        Text("Report Issue")
+                    }
+                }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+            } label: {
+                Text("Help")
+                    .foregroundColor(.primary)
+            }
+            
+            NavigationLink(destination: {}) {
+                Text("About")
+                    .foregroundColor(.primary)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            
+            Button(action: {
+                authenticationvm.signOut()
+            }) {
+                Text("Sign Out")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.leaf, in: RoundedRectangle(cornerRadius: 10))
+            }
+            .foregroundColor(.white)
+            
+        }
+        .padding()
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("Settings")
     }
 }
 
