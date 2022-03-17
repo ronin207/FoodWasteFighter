4f726acaac51f401deb364649572769498c4ec23
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-17 12:34:18 +0800

    Initial Commit

---
+++ FoodWasteFighter/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  FoodWasteFighter
+//
+//  Created by Takumi Otsuka on 17/3/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
